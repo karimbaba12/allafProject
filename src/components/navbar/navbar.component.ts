6d10018e3e448d78e93176a43c93330784321a97
karimbaba12 <babakarim123456@gmail.com>
2024-09-20 19:55:48 +0300

    making home page

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +9,12 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent implements AfterViewInit {
+ ngAfterViewInit(): void {
+    const myCarousel = document.getElementById('carouselExampleControls');
+    const carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 2000,
+      ride: 'carousel'
+    });
+  }
+} 
