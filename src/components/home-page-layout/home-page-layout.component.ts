ae65580ad10da2fd77c63fe654fe7bd1509c2924
karimbaba12 <babakarim123456@gmail.com>
2024-09-20 13:54:40 +0300

    first commit

---
+++ src/components/home-page-layout/home-page-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page-layout.component.html',
+  styleUrl: './home-page-layout.component.css'
+})
+export class HomePageLayoutComponent {
+
+}
