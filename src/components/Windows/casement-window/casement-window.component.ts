58d072476fded5cb17e4c635d43bb13f6931439f
karimbaba12 <babakarim123456@gmail.com>
2024-09-30 15:09:56 +0300

    testing

--- src/components/Windows/casement-window/casement-window.component.ts
+++ src/components/Windows/casement-window/casement-window.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,Inject, PLATFORM_ID,AfterViewInit, NgZone } from '@angular/core';
 import { NavbarComponent } from "../../navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../../footer/footer.component";
+import { isPlatformBrowser } from '@angular/common';
+import { CasementWindow2Component } from "../casement-window2/casement-window2.component";
 @Component({
   selector: 'app-casement-window',
   standalone: true,
-  imports: [NavbarComponent, CommonModule, FooterComponent],
+  imports: [NavbarComponent, CommonModule, FooterComponent, CasementWindow2Component],
   templateUrl: './casement-window.component.html',
   styleUrl: './casement-window.component.css'
 })
-export class CasementWindowComponent {
+export class CasementWindowComponent implements AfterViewInit {
   isFirstDivVisible: boolean = true;
   isSecondDivVisible: boolean = false; 
 
@@ -24,5 +26,27 @@ export class CasementWindowComponent {
     this.isSecondDivVisible = true;
   }
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object , private zone : NgZone) {}
 
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        const body = document.querySelector('body');
+        if (body) {
+          body.classList.add('loaded');
+        }
+      });
+    }
+  }
+  sendMessage() {
+    window.postMessage({ type: 'TEST', text: 'Hello World' }, '*');
+
+    window.addEventListener('message', (event) => {
+      if (event.data.type === 'TEST') {
+        this.zone.run(() => {
+          console.log('Message received: ', event.data.text);
+        });
+      }
+    });
+  }
 }
---
+++ src/components/Windows/casement-window2/casement-window2.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casement-window2',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './casement-window2.component.html',
+  styleUrl: './casement-window2.component.css'
+})
+export class CasementWindow2Component {
+
+}
