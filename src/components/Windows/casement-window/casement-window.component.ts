497aaf9c19d46a23d9856de36aff6efa44294ca5
karimbaba12 <babakarim123456@gmail.com>
2024-10-07 00:01:44 +0300

    sliding window done without responsivity

--- src/components/Windows/casement-window/casement-window.component.ts
+++ src/components/Windows/casement-window/casement-window.component.ts
@@ -2,7 +2,7 @@ import { Component,Inject, PLATFORM_ID,AfterViewInit, NgZone } from '@angular/co
 import { NavbarComponent } from "../../navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../../footer/footer.component";
-import { isPlatformBrowser } from '@angular/common';
+
 import { CasementWindow2Component } from "../casement-window2/casement-window2.component";
 @Component({
   selector: 'app-casement-window',
@@ -11,42 +11,6 @@ import { CasementWindow2Component } from "../casement-window2/casement-window2.c
   templateUrl: './casement-window.component.html',
   styleUrl: './casement-window.component.css'
 })
-export class CasementWindowComponent implements AfterViewInit {
-  isFirstDivVisible: boolean = true;
-  isSecondDivVisible: boolean = false; 
-
-
-  firstButton() {
-    this.isFirstDivVisible = true;
-    this.isSecondDivVisible = false;
-  }
-
-  secondButton() {
-    this.isFirstDivVisible = false;
-    this.isSecondDivVisible = true;
-  }
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object , private zone : NgZone) {}
-
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      setTimeout(() => {
-        const body = document.querySelector('body');
-        if (body) {
-          body.classList.add('loaded');
-        }
-      });
-    }
-  }
-  sendMessage() {
-    window.postMessage({ type: 'TEST', text: 'Hello World' }, '*');
-
-    window.addEventListener('message', (event) => {
-      if (event.data.type === 'TEST') {
-        this.zone.run(() => {
-          console.log('Message received: ', event.data.text);
-        });
-      }
-    });
-  }
+export class CasementWindowComponent  {
+ 
 }
--- src/components/Windows/sliding-window/sliding-window.component.ts
+++ src/components/Windows/sliding-window/sliding-window.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../navbar/navbar.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-sliding-window',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './sliding-window.component.html',
   styleUrl: './sliding-window.component.css'
 })
