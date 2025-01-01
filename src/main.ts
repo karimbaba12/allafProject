020bf2c95c69a318a603c449734a731f72e9d95d
hilalallaf <allafproject@gmail.com>
2025-01-01 20:13:17 +0200

    last commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { bootstrapApplication } from '@angular/platform-browser';
 // import { appConfig } from './app/app.config';
 // import { AppComponent } from './app/app.component';
@@ -6,26 +5,12 @@
 // bootstrapApplication(AppComponent, appConfig)
 //   .catch((err) => console.error(err));
 
-=======
->>>>>>> 667df993ee86f11a32f22ec391b8ad440c13b248
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 
-<<<<<<< HEAD
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [...(appConfig.providers || []), provideHttpClient()],
 }).catch((err) => console.error(err));
-=======
-
-bootstrapApplication(AppComponent, {
-  ...appConfig, 
-  providers: [
-    ...appConfig.providers || [], 
-    provideHttpClient() 
-  ]
-})
-.catch((err) => console.error(err));
->>>>>>> 667df993ee86f11a32f22ec391b8ad440c13b248
